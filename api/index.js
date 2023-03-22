5bdcdb648fff738da8b1266e238d5b7a73a70267
Ar3secchim <renarasecchim@gmail.com>
2023-03-22 14:41:12 -0300

    feat: config package json and vercel

--- api/index.js
+++ api/index.js
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('🔥 Server running on port http://localhost:3000');
+app.listen(3002, () => {
+  console.log('🔥 Server running on port http://localhost:3002');
 })
